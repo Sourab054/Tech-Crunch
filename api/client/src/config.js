3aec0763383e8ed7d08c79e34e107e5d1cc90080
Sourab Patil <sourab054@gmail.com>
2022-04-18 20:31:45 +0530

    Deployment

--- api/client/src/config.js
+++ api/client/src/config.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://tech-crunch.herokuapp.com/api",
+  baseURL: "https://tech-crunch.herokuapp.com/api/",
 });
